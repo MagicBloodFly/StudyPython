c22358056a1738fcaf645fcd01c91edf722f441b
MagicBloodFly <2290657746@qq.com>
2020-10-29 22:32:25 +0800

    上传有用的作业

--- Item/写作业用.py
+++ Item/写作业用.py
@@ -6,3 +6,27 @@ z = ("%.6f" % z)
 
 print("y="+z)
 
+# 作业
+n = int(input(""))
+
+arg = []
+
+number = 4
+
+var = 0
+
+while number > 0:
+
+    number -= 1
+
+    s = n**var
+
+    var += 1
+
+    arg.append(str(s))
+
+
+print(" ".join(arg))
+
+
+
